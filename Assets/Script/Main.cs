50ba521a9cd1502a39e1a69bca7bb5b59d604598
ueda.h11-7@gmail.com <>
2020-12-17 01:37:26 +0900

    add unity project

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Main : MonoBehaviour
+{
+    // entrypoint.
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private void EntryPoint()
+    {
+        Debug.Log("entry");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
